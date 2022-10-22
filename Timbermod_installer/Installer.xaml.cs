9911971d5c5c9547dbff547dd9f4083ee4c6918d
KYPremco <KYPremco@gmail.com>
2022-10-22 12:17:38 +0200

    Initial commit

---
+++ Timbermod_installer/Installer.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Timbermod_installer.LogSystem;
+
+namespace Timbermod_installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Installer : Window
+    {
+        public Installer()
+        {
+            DataContext = LogService.Instance;
+            InitializeComponent();
+        }
+    }
+}
---
+++ Timbermod_installer/LogSystem/LogService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Timbermod_installer.LogSystem
+{
+    public class LogService
+    {
+        private static LogService _logService;
+
+        public static LogService Instance => _logService ??= new LogService();
+
+        public ObservableCollection<string> Logs { get; } = new();
+
+        public static void Send(string message)
+        {
+            _logService.Logs.Add(message);
+        }
+    }
+}
